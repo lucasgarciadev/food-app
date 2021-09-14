d3dc43cff22656785d3c010d99e3413274e559ae
lucasgarciadev <lukas.garcia52@gmail.com>
2021-09-13 23:20:15 -0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Menu, PageBody } from './AppStyled';
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+import MenuItem from './components/MenuItem'
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+            <h1>Oi, {name}</h1>
+            <Container>
+                
+                <Menu>
+                    <MenuItem icon="/assets/store.png" link="/" />
+                    <MenuItem icon="/assets/order.png" link="/orders" />
+                    <MenuItem icon="/assets/profile.png" link="/profile" />
+                </Menu>
+
+                <PageBody>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>
+                
+                {/* <Cart>
+                
+                </Cart> */}
+
+            </Container>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/AppStyled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display:flex;
+    height: 100vh;
+    background-color:#FF0000;
+`;
+export const Menu = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    background-color: #136713;
+    width:80px;
+`;
+
+export const PageBody = styled.div`
+    display:flex;
+    background-color: #0000FF;
+    flex:1;
+`;
